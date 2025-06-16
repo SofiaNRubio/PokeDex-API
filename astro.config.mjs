b25c80ba37ba4db96677d1c0f6e62df109358e11
SofiaNRubio <damarishrubio@gmail.com>
2025-06-16 19:33:25 -0300

    refactor: Cleaned dependencies and reinstalled for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
-
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel'; // ⬅️ ¡Así es como debe ser ahora!
+import vercel from '@astrojs/vercel'; // La importación debe ser así
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // ⬅️ Paso 2a: Configura el tipo de salida como 'server'
-  adapter: vercel(), // ⬅️ Paso 2b: Usa el adaptador de Vercel
+  output: 'server',
+  adapter: vercel(),
 });
