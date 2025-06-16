c3351f0f30ec7e7248bcce95b622e4fe835953e1
SofiaNRubio <damarishrubio@gmail.com>
2025-06-16 19:25:30 -0300

    add: vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,19 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+
+});
+
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/server'; // ⬅️ Paso 1: Importa el adaptador de Vercel
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server', // ⬅️ Paso 2a: Configura el tipo de salida como 'server'
+  adapter: vercel(), // ⬅️ Paso 2b: Usa el adaptador de Vercel
+});
